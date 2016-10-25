e10b7486776ec051361e5db6a7662558c6782000
PavanAv <pavanaluri2011@gmail.com>
2016-10-25 16:50:38 +0530

    Session6Assignment4
    Session6Assignment4

---
+++ S6Assignment4/src/Student.java
@@ -0,0 +1,13 @@
+public abstract class Student {
+	protected int rollNumber;	
+	protected int regNumber;		
+	protected String name;				
+	public Student(String name, int rollNumber, int regNumber) {	        
+	this.name = name;        
+	this.rollNumber = rollNumber;        
+	this.regNumber = regNumber;    
+	}		
+	public abstract float CalculateResult();	
+	public abstract void getSubject();	
+	public abstract void showDetails(boolean printValues);
+}
